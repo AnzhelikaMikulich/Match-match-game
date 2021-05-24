9deb7fdd38279b3f2b500d1d68c9ab5a6ab5a683
AnzhelikaMikulich <angelika.flyorko@gmail.com>
2021-05-24 23:49:57 +0300

    feat: add a field with cards for the game (6 * 6)

---
+++ match-match-game/webpack-demo/src/components/cards-field/cards-field.ts
@@ -0,0 +1,25 @@
+import { BaseComponent } from '../base-component';
+import { Card } from '../card/card';
+import './cards-field.scss';
+
+const SHOW_TIME = 30;
+export class CardsField extends BaseComponent {
+  private cards: Card[] = [];
+
+  constructor() {
+    super('div', ['cards-field']);
+  }
+
+  clear() {
+    this.cards = [];
+    this.element.innerHTML = '';
+  }
+
+  addCards(cards: Card[]) {
+    this.cards = cards;
+    this.cards.forEach((card) => this.element.appendChild(card.element));
+    setTimeout(() => {
+      this.cards.forEach((card) => card.flipToBack());
+    }, SHOW_TIME * 1000);
+  }
+}
